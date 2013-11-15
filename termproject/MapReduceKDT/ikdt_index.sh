4aeb9fcae41d0e2706d13011cf11775c65b5de54
Yves Xie <yxi389@gmail.com>
2013-11-15 14:14:36 -0600

    bash scripts

--- termproject/MapReduceKDT/ikdt_index.sh
+++ termproject/MapReduceKDT/ikdt_index.sh
@@ -1,19 +1,17 @@
+#!/bin/bash
+
 # modify data in ikdt_config necessary.
 
-python ikdt_index.py > ikdt_index_output &
+OUTPUT=ikdt_index_output.py
+python ikdt_index.py > $OUTPUT &
 SERVER_PID=$!
 sleep 1
-python mincemeat.py -p changeme -P 11230 localhost &
-python mincemeat.py -p changeme -P 11231 localhost &
-python mincemeat.py -p changeme -P 11232 localhost &
-python mincemeat.py -p changeme -P 11233 localhost &
-python mincemeat.py -p changeme -P 11234 localhost &
-python mincemeat.py -p changeme -P 11235 localhost &
-python mincemeat.py -p changeme -P 11236 localhost &
-python mincemeat.py -p changeme -P 11237 localhost &
-python mincemeat.py -p changeme -P 11238 localhost &
-python mincemeat.py -p changeme -P 11239 localhost &
-python mincemeat.py -p changeme -P 11240 localhost &
+python mincemeat.py -p changeme -P 11230 localhost 2>/dev/null&
+python mincemeat.py -p changeme -P 11231 localhost 2>/dev/null&
+python mincemeat.py -p changeme -P 11232 localhost 2>/dev/null&
+python mincemeat.py -p changeme -P 11233 localhost 2>/dev/null&
+python mincemeat.py -p changeme -P 11234 localhost 2>/dev/null&
 sleep 1
-#kill $! 2>/dev/null
+kill $! 2>/dev/null
+printf "tree = " | cat - $OUTPUT > /tmp/tempfile && mv /tmp/tempfile $OUTPUT; 
 # make sure the program finishes.
