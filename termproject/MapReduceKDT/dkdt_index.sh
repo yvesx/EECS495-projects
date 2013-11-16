556e5e4f8531ee3951fe243655f005f7c2c9f3a5
Yves Xie <yxi389@gmail.com>
2013-11-15 20:05:04 -0600

    run dkdt indexer

---
+++ termproject/MapReduceKDT/dkdt_index.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# modify data in ikdt_config necessary.
+
+OUTFTR=dkdt_chunk_ends.py
+OUTIDX=dkdt_index_output.py
+python dkdt_feature.py > $OUTFTR
+printf "chunk_ends = " | cat - $OUTFTR > /tmp/tempfile && mv /tmp/tempfile $OUTFTR; 
+
+python dkdt_index.py > $OUTIDX &
+SERVER_PID=$!
+sleep 1
+python mincemeat.py -p dkdt -P 11230 localhost &
+python mincemeat.py -p dkdt -P 11231 localhost &
+python mincemeat.py -p dkdt -P 11232 localhost &
+python mincemeat.py -p dkdt -P 11233 localhost &
+python mincemeat.py -p dkdt -P 11234 localhost &
+sleep 1
+kill $! 2>/dev/null
+printf "tree = " | cat - $OUTIDX > /tmp/tempfile && mv /tmp/tempfile $OUTIDX; 
+# make sure the program finishes.
